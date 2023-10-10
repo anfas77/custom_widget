23011e64b9fd8ef246deecf1fc3f36dac8d79de7
anfas77 <anfas596@gmail.com>
2023-10-10 12:34:34 +0530

    abcd

---
+++ lib/customwdgt.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class custumwdget extends StatelessWidget {
+  String text1;
+  Color colorContai;
+  custumwdget({required this.colorContai, required this.text1});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: colorContai,
+      child: Center(child: Text(text1)),
+    );
+  }
+}
